868fbfd12aef4ae8a119ca42eea9c6dad4a0237d
Facundo Perez <facundoperez94@gmail.com>
2021-02-08 11:39:15 -0300

    added favicon

--- hcs_sudameris/__manifest__.py
+++ hcs_sudameris/__manifest__.py
@@ -23,6 +23,7 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
+        'views/website_templates.xml',
         'views/webclient_templates.xml',
         'views/employee.xml',
         'views/salary_movement.xml',
